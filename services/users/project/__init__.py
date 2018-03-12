b4d6a15370ef9d598ed87633fcd60d54feabf1f6
George Victor <victormanakkat@gmail.com>
2018-03-12 16:53:20 +0530

    Added the docker machine and docker compose and containerized the users module

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,11 +1,20 @@
 
+import os
+# import sys
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
 
 # set configuration - is it relative from the flask cli is (manage.py)?
-app.config.from_object('project.config.DevelopmentConfig')
+# app.config.from_object('project.config.DevelopmentConfig')
+
+# set config from the environment variable define in the docker-compose yml file
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# prints to the console, when run a docker-compose...logs command
+# print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
