47aeefa742ff70d74d5725e734430c4cad3f5abf
George Victor <victormanakkat@gmail.com>
2018-03-13 17:34:35 +0530

    Added unit testing for users and configuration

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,15 +1,27 @@
 
+import unittest
 from flask.cli import FlaskGroup
 from project import app, db
 
 cli = FlaskGroup(app)
 
-# Register a new cli command 'recreate_db'
+# Register a new cli command 'recreate_db'.
+# Run unsing 'docker-compose -f docker-compose-dev.yml run users python manage.py recreate_db'
 @cli.command()
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+# Register a new cli command 'test'
+@cli.command()
+def test():
+    """ Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 if __name__ == '__main__':
     cli()
